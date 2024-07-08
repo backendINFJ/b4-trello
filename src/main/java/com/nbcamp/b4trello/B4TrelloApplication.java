e643ddd722e33604224722aa4198f7fc96470512
yoonjae <backendINFJ@gmail.com>
2024-07-09 08:53:36 +0900

    Initial commit

---
+++ src/main/java/com/nbcamp/b4trello/B4TrelloApplication.java
@@ -0,0 +1,13 @@
+package com.nbcamp.b4trello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B4TrelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(B4TrelloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nbcamp/b4trello/B4TrelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbcamp.b4trello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B4TrelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
