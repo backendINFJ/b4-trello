b898132d0533c2dd56773268f83a6266cc6d5f2d
Delphinium52 <63048093+Delphinium52@users.noreply.github.com>
2024-07-11 17:37:11 +0900

    feat AuthService

---
+++ src/main/java/com/nbcamp/b4trello/service/AuthService.java
@@ -0,0 +1,75 @@
+package com.nbcamp.b4trello.service;
+
+import com.nbcamp.b4trello.dto.TokenDTO;
+import com.nbcamp.b4trello.entity.RefreshToken;
+import com.nbcamp.b4trello.jwt.JwtEnum;
+import com.nbcamp.b4trello.jwt.JwtUtil;
+import com.nbcamp.b4trello.repository.RefreshTokenRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 로그인 인증 관련 서비스
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AuthService implements LogoutHandler {
+    /**
+     * 관련 클래스 호출
+     */
+    private final JwtUtil jwtUtil;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+
+    /**
+     * 토큰 재발급 메서드
+     * @param refreshToken
+     * @return
+     */
+    @Transactional
+    public TokenDTO reissue(String refreshToken) {
+        Optional<RefreshToken> token = refreshTokenRepository.findByToken(refreshToken);
+        if(token!=null && !token.get().getRefreshToken().equals(refreshToken)){
+            throw new RuntimeException("잘못된 토큰입니다.");
+        }
+        Authentication authentication = jwtUtil.getAuthentication(refreshToken.substring(7));
+        TokenDTO tokenDto = jwtUtil.createToken(authentication);
+        token.get().updateRefreshToken(tokenDto.getRefreshToken());
+        return tokenDto;
+    }
+
+    /**
+     * 로그아웃 메서드
+     * @param request
+     * @param response
+     * @param authentication
+     */
+    @Transactional
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response , Authentication authentication) {
+        String authHeader = request.getHeader(JwtEnum.ACCESS_TOKEN.getValue());
+
+        if (authHeader == null && !authHeader.startsWith(JwtEnum.GRANT_TYPE.getValue())) {
+            throw new RuntimeException("알수 없는 access token.");
+        }
+        String accessToken = authHeader.substring(7);
+        String username = jwtUtil.getUsername(accessToken);
+        RefreshToken refreshToken = refreshTokenRepository.findByToken(username).orElse(null);
+        if(refreshToken == null) {
+            throw new RuntimeException("잘못된 토큰");
+        }
+        refreshTokenRepository.delete(refreshToken);
+    }
+
+
+
+}
