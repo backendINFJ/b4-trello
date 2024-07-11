81c5d93da76290d7f235202c2179b4ae9ba5e894
backendINFJ <backendINFJ@gmail.com>
2024-07-12 05:03:17 +0900

    chore :  ResponseEnum 상수 추가

--- src/main/java/com/nbcamp/b4trello/dto/ResponseEnum.java
+++ src/main/java/com/nbcamp/b4trello/dto/ResponseEnum.java
@@ -11,7 +11,8 @@ public enum ResponseEnum {
 
 	CREATE_CARD("카드 생성 완료", HttpStatus.CREATED),
 	CREATE_BOARD("보드 생성 완료", HttpStatus.CREATED),
-	READ_BOARD("보드 전체조회 성공",HttpStatus.OK);
+	READ_BOARD("보드 전체조회 완료",HttpStatus.OK),
+	UPDATE_BOARD("보드 수정 완료",HttpStatus.OK);
 
 	private final String message;
 	private final HttpStatus httpStatus;
