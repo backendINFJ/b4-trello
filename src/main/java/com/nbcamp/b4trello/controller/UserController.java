c7ea2b46edb43ec3a7ba43a330e57115f94ac791
Delphinium52 <63048093+Delphinium52@users.noreply.github.com>
2024-07-12 10:31:36 +0900

    feat user controller
    1차 유저 컨트롤러

--- src/main/java/com/nbcamp/b4trello/controller/UserController.java
+++ src/main/java/com/nbcamp/b4trello/controller/UserController.java
@@ -1,4 +1,46 @@
 package com.nbcamp.b4trello.controller;
 
+import com.nbcamp.b4trello.dto.UserRequestDTO;
+import com.nbcamp.b4trello.dto.UserUpdateDTO;
+import com.nbcamp.b4trello.security.UserDetailsImpl;
+import com.nbcamp.b4trello.service.UserService;
+import jakarta.validation.Valid;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/user")
+@RestController
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/")
+    public ResponseEntity<String> createUser(@Valid @RequestBody UserRequestDTO userDto) {
+        return userService.createUser(userDto);
+    }
+
+    @PatchMapping("/{userId}")
+    public ResponseEntity<String> updateUser(@PathVariable("userId") Long userId, @Valid @RequestBody UserUpdateDTO updateDTO, @AuthenticationPrincipal
+    UserDetailsImpl userDetails) {
+        return userService.updateUser(userId, updateDTO, userDetails.getUser());
+    }
+
+    @PostMapping("/delete/{userId}")
+    public ResponseEntity<String> deleteUser(@PathVariable("userId") Long userId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return userService.deleteUser(userId, userDetails.getUser());
+    }
 }
