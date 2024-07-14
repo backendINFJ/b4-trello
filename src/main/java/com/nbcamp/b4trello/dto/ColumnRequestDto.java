38f08b0afbfaae8f8b92226ce074a4fa01ac5358
Been777 <167050271+Been777@users.noreply.github.com>
2024-07-14 23:18:31 +0900

    REFACTOR : 컬럼 순서 이동
    : RequestDto 메서드 삭제

--- src/main/java/com/nbcamp/b4trello/dto/ColumnRequestDto.java
+++ src/main/java/com/nbcamp/b4trello/dto/ColumnRequestDto.java
@@ -1,34 +1,15 @@
 package com.nbcamp.b4trello.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Collections;
 import java.util.List;
 
+@Getter
+@Setter
 public class ColumnRequestDto {
-        private Long boardId;
-        private Long columnId;
-        private String columnTitle;
-
-        public Long getBoardId() {
-            return boardId;
-        }
-
-        public void setBoardId(Long boardId) {
-            this.boardId = boardId;
-        }
-
-        public List<Long> getColumnId() {
-            return Collections.singletonList(columnId);
-        }
-
-        public void setColumnIds(Long columnId) {
-            this.columnId = columnId;
-        }
-
-        public String getColumnTitle() {
-            return columnTitle;
-        }
-
-        public void setColumnTitle(String columnTitle) {
-            this.columnTitle = columnTitle;
-        }
+    private Long boardId;
+    private List<Long> columnIds;
+    private String columnTitle;
 }
